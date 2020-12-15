f94874cde41aafde7c8ab1110a45dd92365ca42a
JoauMSuD <joaumsud@hotmail.com>
2020-12-14 22:34:53 -0300

    Aula de Variáveis.

---
+++ variaveis.js
@@ -0,0 +1,43 @@
+/* 1)Exercício: Declarando Variáveis
+Uma variável é um espaço de memória para armazenar valores. Podemos criá-los e, por sua vez, também podemos atribuir-lhes um certo valor.
+
+Em JavaScript, podemos declará-los usando a palavra var  e depois o nome que queremos dar.
+Sua tarefa será declarar duas variáveis: uma com o nome idade  e o outro com o nome peso.
+
+    var idade;
+    var peso; */
+
+/*2) Exercício: Atribuindo valores a variáveis
+Já vimos como declarar uma variável! Mas como podemos armazenar valores dentro dela?
+
+Para armazenar um valor dentro de uma variável, iremos utilizar um símbolo =   seguido do valor que queremos armazenar. Em programação, 
+chamamos esse procedimento de atribuição. Exemplo: var meses = 12;
+var quantidadeDeAlunos = 30;
+
+Declare duas variáveis chamadas  idade  e  peso,  e atribua um valor numérico a ambas.*/ 
+
+var idade = 30;
+var peso = 80;
+
+/*3)Exercício: A diferença entre var e let
+Vamos repetir o mesmo passo que antes, mas em vez de var, você deve usar let. 
+Para isso, você terá que modificar as seguintes variáveis ​​declaradas com var e alterá-las para let.
+
+let nome;
+let sobrenome;
+let idade;
+let telefone;
+let sabeProgramar;
+*/
+
+/* 4) As constantes
+Agora pense nesse exemplo, temos que validar algumas pessoas pelas suas idades. 
+Nosso critério será 18 anos como idade miníma e 99 como idade máxima. Ninguém pode mudar essas definições. 
+Para que isso ocorra de fato iremos escrever constantes. Vale dizer que, por questões de boas práticas,  
+é comum utilizar nome com letras maiúsculas na hora de definir uma constante.
+
+Bom agora, você deve criar um constante chamada IDADE_MINIMA, e outra chamada IDADE_MAXIMA. Que por sua vez, devem ter os valores 18 e 99 respectivamente.
+
+*/
+const idade_minima = 18;
+const idade_maxima = 99;
